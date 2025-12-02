22930fe941103649162c5d9f3932ba2ac9815cdd
mithiladilshan9977 <dilshanwickramaarachchi99@gmail.com>
2025-12-02 10:49:23 +0530

    feat: Add update product route and remove commented-out routes in server configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,41 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 import { adminIds, memberIds, productIds, staffUserIds } from '../shared/mocks/route-params';
 
 export const serverRoutes: ServerRoute[] = [
-  // {
-  //   path: 'auth/email-auth/:adminid',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return adminIds.map(adminid => ({ adminid }));
-  //   }
-  // },
-  // {
-  //   path: 'home/create-members/:memberid',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return memberIds.map(memberid => ({ memberid }));
-  //   }
-  // },
-  // {
-  //   path: 'home/update-staff-user/:uid/:adminid',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return staffUserIds.map(({ uid, adminid }) => ({ uid, adminid }));
-  //   }
-  // },
-  // {
-  //   path: 'home/update-shop-item/:productid',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return productIds.map(productid => ({ productid }));
-  //   }
-  // },
-  // {
-  //   path: 'home/selling-center/:productid',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     return productIds.map(productid => ({ productid }));
-  //   }
-  // },
+  {
+    path: 'home/update-product/:productid/:userid',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+
+      const productUserCombos = [
+        { productid: 'prod1', userid: 'user1' },
+        { productid: 'prod2', userid: 'user2' },
+      ];
+      return productUserCombos;
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
